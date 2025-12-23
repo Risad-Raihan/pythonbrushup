dd9d8bdb1c5e3aaa66be4625fd03935568ac9b1a
Risad Raihan <rrmalik66@gmail.com>
2025-12-23 16:34:36 +0600

    tavaily_client.py created

---
+++ OOP/tavily_client.py
@@ -0,0 +1,39 @@
+import os
+from typing import List, Dict
+from tavily import TavilyClient
+
+class TavilySearchClient:
+    def __init__(self, api_key: str | None = None):
+        #loaded from .emv
+        self.api_key = api_key or os.getenv("Tavily_API_KEY")
+
+        if not self.api_key:
+            raise ValueError("API not found on env")
+
+        self.client =  TavilyClient(api_key=self.api_key)
+
+    
+    def search(
+        self,
+        query:str,
+        max_result: int = 4,
+        search_depth: "general"
+    ) -> List[Dict]:
+        """
+        tavily web serach and return raw result:
+        - title
+        - url
+        - content
+        - score
+        """
+
+        response = self.client.search(
+            query = query,
+            max_result = max_result,
+            search_depth = search_depth,
+            topic = topic,
+            include_answer =  False,
+            include_raw_content = False
+        )
+
+        return response.get("results", [])
