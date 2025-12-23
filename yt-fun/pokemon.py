a0de4897e029a9327ba292ac566c041cee81719b
Risad Raihan <rrmalik66@gmail.com>
2025-12-23 14:05:42 +0600

    pokemaneeeee v2

--- yt-fun/pokemon.py
+++ yt-fun/pokemon.py
@@ -7,18 +7,14 @@ def get_pokemon_info(name):
     response = requests.get(url)
 
     if response.status_code == 200:
-        pokemon_data = response.json()
-        return pokemon_data
+        return url, response.json()
     else:
-        print(f"Failed to retriecve data {response.status_code}")
-
+        print(f"Failed to retrieve data {response.status_code}")
+        return None, None
 
 pokemon_name = "pikachu"
-pokemon_info= get_pokemon_info(pokemon_name)
+url, pokemon_info = get_pokemon_info(pokemon_name)
 
 if pokemon_info:
-    print(f"Name:{pokemon_info["name"].capitalize()}")
-    print(f"Id:{pokemon_info["id"]}")
-    print(f"Height:{pokemon_info["height"]}")
-    print(f"Weight:{pokemon_info["weight"]}")
-    
+    print("URL:", url)
+    print(f"Name: {pokemon_info['name'].capitalize()}")
